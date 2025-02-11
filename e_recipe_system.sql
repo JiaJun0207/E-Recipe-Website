512081ac3acf8e625a12f0b7dd78194b214b3e1f
Chen Yong Qi <yongqi218@gmail.com>
2025-02-11 14:06:32 +0800

    Update e_recipe_system.sql

--- e_recipe_system.sql
+++ e_recipe_system.sql
@@ -137,10 +137,18 @@ INSERT INTO `meal_type` (`typeID`, `mealType`) VALUES
 --
 
 CREATE TABLE `rating` (
-  `ratingID` int(11) NOT NULL,
-  `ratingNum` int(11) NOT NULL
+    ratingID INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    ratingNum INT(11) NOT NULL,
+    ratingText VARCHAR(50) NOT NULL
+  PRIMARY KEY (`ratingID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
->>>>>>> 8f8f97da3b49ed7fe6465820d6dcd9e7ac1a53ba
+
+INSERT INTO `rating` (`ratingNum`, `ratingText`) VALUES
+(1, 'Poor'),
+(2, 'Fair'),
+(3, 'Good'),
+(4, 'Very Good'),
+(5, 'Excellent');
 
 -- --------------------------------------------------------
 
